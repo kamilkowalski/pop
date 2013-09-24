9500a10c5f23f7daa9f248d85fe76c7c08860c27
Blazej Dziemianko <blazej@webtotal.no>
2013-09-24 13:00:31 +0200

    calendar screen

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $("#showPop1").on("click", function(){
   $("#myPop1").pop("show");
 });
 
+$("#showPop3").on("click", function(){
+  $("#myPop3").pop("show", {load: "ajax_calendar.html"});
+});
+
 $("#showPop2").on("click", function(){
   $("#myPop2").pop("show", {load: "ajax_contents.html"});
 });
