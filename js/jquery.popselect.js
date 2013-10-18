2c79becffcb1d5725077daf46e69176acfa4a726
Kamil Kowalski <kowalski.tychy@gmail.com>
2013-10-18 10:52:38 +0200

    Add wo.popselect to element data to prevent double creation of a popselect

--- js/jquery.popselect.js
+++ js/jquery.popselect.js
@@ -15,57 +15,62 @@
 
       var that = $(this);
 
-      // Set up the elements
-      var ps = $("<div>", {"class": "popselect"});
-      var display = $("<div>", {"class": "popselect-display"});
-      var handle = $("<a>", {href: "#", "class": "popselect-handle"}).append($("<i>"));
-      var dropdown = $("<ul>", {"class": "popselect-dropdown"});
+      if(!that.data("wo.popselect")){
+        // Set up the elements
+        var ps = $("<div>", {"class": "popselect"});
+        var display = $("<div>", {"class": "popselect-display"});
+        var handle = $("<a>", {href: "#", "class": "popselect-handle"}).append($("<i>"));
+        var dropdown = $("<ul>", {"class": "popselect-dropdown"});
 
-      // Append the new select
-      ps.append(display, handle, dropdown);
-      that.after(ps);
+        // Append the new select
+        ps.append(display, handle, dropdown);
+        that.after(ps);
 
-      // Hide the original select
-      that.hide();
+        // Hide the original select
+        that.hide();
 
-      // Get the options from the select box and create list elements
-      that.find("option").each(function(){
-        var li = $("<li>", {data: {value: $(this).val()}});
-        dropdown.append(li);
-        
-        li.text($(this).text());
-        li.data("value", $(this).val());
-      });
+        // Get the options from the select box and create list elements
+        that.find("option").each(function(){
+          var li = $("<li>", {data: {value: $(this).val()}});
+          dropdown.append(li);
+          
+          li.text($(this).text());
+          li.data("value", $(this).val());
+        });
 
-      // Set up events
-      handle.add(display).on("click", function(e){
-        e.preventDefault();
-        dropdown.slideToggle(50);
-        ps.toggleClass("active");
-      });
+        // Set up events
+        handle.add(display).on("click", function(e){
+          e.preventDefault();
+          dropdown.slideToggle(50);
+          ps.toggleClass("active");
+        });
 
-      dropdown.on("click", "li", function(){
-        that.val($(this).data("value"));
-        that.trigger("change");
-        display.text($(this).text());
-        $(this).siblings().removeClass("active").end().addClass("active");
-        dropdown.slideUp(50);
-        ps.removeClass("active");
-      });
-
-      $(document).on("click", function(e){
-        if(!ps.is(e.target) && ps.has(e.target).length == 0){
+        dropdown.on("click", "li", function(){
+          that.val($(this).data("value"));
+          that.trigger("change");
+          display.text($(this).text());
+          $(this).siblings().removeClass("active").end().addClass("active");
           dropdown.slideUp(50);
           ps.removeClass("active");
-        }
-      });
+        });
 
-      that.on("change", function(){
+        $(document).on("click", function(e){
+          if(!ps.is(e.target) && ps.has(e.target).length == 0){
+            dropdown.slideUp(50);
+            ps.removeClass("active");
+          }
+        });
+
+        that.on("change", function(){
+          updatePopselect(that, display, dropdown);
+        });
+
+        // Run the update procedure to get the current selection
         updatePopselect(that, display, dropdown);
-      });
 
-      // Run the update procedure to get the current selection
-      updatePopselect(that, display, dropdown);
+        // Save the popselect creation in data, so that it's not created again
+        that.data("wo.popselect", true);
+      }
     });
   }
 
